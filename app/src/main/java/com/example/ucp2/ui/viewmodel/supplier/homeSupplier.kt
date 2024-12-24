f20fcb2a03d60f5dd2f6ba683ea5a9d5e6d8fb01
WisnuDwiSeisno <wseisno37@gmail.com>
2024-12-24 10:24:23 +0700

    Menambahkan StateFlow untuk mengambil data supplier dengan penanganan loading dan error

--- app/src/main/java/com/example/ucp2/ui/viewmodel/supplier/homeSupplier.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/supplier/homeSupplier.kt
@@ -1,12 +1,49 @@
 package com.example.ucp2.ui.viewmodel.supplier
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.ucp2.data.entity.Supplier
 import com.example.ucp2.repository.repoSupp
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
 
 class homeSupplier(
     private val repoSupp: repoSupp
 ) : ViewModel() {
+    val homeUiState: StateFlow<HomeUiState> = repoSupp.getAllSupp()
+        .filterNotNull()
+        .map {
+            HomeUiState(
+                listSupp = it.toList(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(HomeUiState(isLoading = true))
+            delay(900)
+        }
+        .catch {
+            emit(
+                HomeUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi Kesalahan"
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = HomeUiState(
+                isLoading = true,
+            )
+        )
 }
 
 data class HomeUiState(
