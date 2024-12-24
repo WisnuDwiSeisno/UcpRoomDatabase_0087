8dc5e255219a9b28d8e5d41f0ab7e5378fb49fba
WisnuDwiSeisno <wseisno37@gmail.com>
2024-12-24 10:34:30 +0700

    Menambahkan fungsi untuk memperbarui state, validasi form, dan menyimpan data supplier dengan penanganan pesan kesalahan atau sukses

--- app/src/main/java/com/example/ucp2/ui/viewmodel/supplier/insertSupplier.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/supplier/insertSupplier.kt
@@ -1,10 +1,69 @@
 package com.example.ucp2.ui.viewmodel.supplier
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.ucp2.data.entity.Supplier
 import com.example.ucp2.repository.repoSupp
+import kotlinx.coroutines.launch
+
 
 class insertSupplier(private val repoSupp: repoSupp) : ViewModel() {
+
+    var uiState by mutableStateOf(SuppUIState())
+
+    // Memperbarui state berdasarkan input pengguna
+    fun updateState(supplierEvent: supplierEvent) {
+        uiState = uiState.copy(
+            supplierEvent = supplierEvent,
+        )
+    }
+
+    // Validasi data import pengguna
+    private fun validateFields(): Boolean {
+        val event = uiState.supplierEvent
+        val errorState = FormErrorState(
+            id = if (event.id.isNotEmpty()) null else "id tidak boleh kosong",
+            nama = if (event.nama.isNotEmpty()) null else "Nama tidak boleh kosong",
+            kontak = if (event.kontak.isNotEmpty()) null else "Kontak tidak boleh kosong",
+            alamat = if (event.alamat.isNotEmpty()) null else "Alamat tidak boleh kosong"
+        )
+        uiState = uiState.copy(isEntryValid = errorState)
+        return errorState.isValid()
+    }
+
+    //Menyimpan data ke repository
+    fun saveData() {
+        val currentEvent = uiState.supplierEvent
+
+        if (validateFields()) {
+            viewModelScope.launch {
+                try {
+                    repoSupp.insertSupp(currentEvent.toSupplierEntity())
+                    uiState = uiState.copy(
+                        snackBarMessage = "Data berhasil disimpan",
+                        supplierEvent = supplierEvent(), //Reset input form
+                        isEntryValid = FormErrorState() // Reset error state
+                    )
+                } catch (e: Exception) {
+                    uiState = uiState.copy(
+                        snackBarMessage = "Data gagal disimpan"
+                    )
+                }
+            }
+        } else {
+            uiState = uiState.copy(
+                snackBarMessage = "Input tidak valid. Periksa kembali data anda."
+            )
+        }
+    }
+
+    //Reset pesan Snackbar setelah ditampilkan
+    fun resetSnackBarMessage() {
+        uiState = uiState.copy(snackBarMessage = null)
+    }
 }
 
 
