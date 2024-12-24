607ed49de095e170ea5497983c765ff3468bd7ec
WisnuDwiSeisno <wseisno37@gmail.com>
2024-12-24 10:45:53 +0700

    Menambahkan kelas TokoApp untuk menyimpan instance ContainerApp yang digunakan dalam aplikasi

--- app/src/main/java/com/example/ucp2/TokoApp.kt
+++ app/src/main/java/com/example/ucp2/TokoApp.kt
@@ -1,2 +1,15 @@
 package com.example.ucp2
+import android.app.Application
+import com.example.ucp2.dependenciesinjection.containerApp
 
+class TokoApp : Application() {
+    // Fungsinya untuk menyimpan instance ContainerApp
+    lateinit var containerApp: containerApp
+
+    override fun onCreate() {
+        super.onCreate()
+        // Membuat instance ContainerApp
+        containerApp = containerApp(this)
+        // instance adalah object yang dibuat dari class
+    }
+}
